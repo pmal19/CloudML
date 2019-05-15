d7b4559b6158e831c76e801013c5e39e55078ce4
Pramit Mallick <pramitmallick@gmail.com>
2019-05-15 15:17:12 -0400

    t

--- mpi_dist_trial.py
+++ mpi_dist_trial.py
@@ -301,6 +301,8 @@ def run_demo(demo_fn, world_size):
 
 if __name__ == "__main__":
     # init_processes(0, 0, run, backend='gloo')
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
     dist.init_process_group(backend="gloo")
     rank = dist.get_rank()
     wsize = dist.get_world_size()
